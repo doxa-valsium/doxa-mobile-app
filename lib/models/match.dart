1c658070b049cc10756dbd107666ee43b27cef5d
Abuzar Rasool <abuzar@oraan.com>
2022-06-04 06:33:47 +0500

    Update match.dart

--- lib/models/match.dart
+++ lib/models/match.dart
@@ -1,18 +1,19 @@
 import 'package:doxa_mobile_app/models/candidate.dart';
 import 'package:doxa_mobile_app/models/job.dart';
+import 'package:equatable/equatable.dart';
 
 enum MatchStatus { initiated, notInititaed, closed }
 
 enum ClosingReason { automatic, closedByRecruiter, closedByCandidate }
 
-class Match {
-  String matchId;
-  Job job;
-  Candidate candidate;
-  MatchStatus matchStatus;
-  ClosingReason? closingReason;
-  DateTime createdAt;
-  Match({
+class Match extends Equatable {
+  final String matchId;
+  final Job job;
+  final Candidate candidate;
+  final MatchStatus matchStatus;
+  final ClosingReason? closingReason;
+  final DateTime createdAt;
+  const Match({
     required this.matchId,
     required this.job,
     required this.candidate,
@@ -20,4 +21,7 @@ class Match {
     this.closingReason,
     required this.createdAt,
   });
+  
+  @override
+  List<Object?> get props => [matchId, job, candidate, matchStatus, closingReason, createdAt];
 }
